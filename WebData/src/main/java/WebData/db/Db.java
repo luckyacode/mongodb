9e25fd88430b8be3284ac0b4c64f8ef492090283
praveen <lifesstudy@gmail.com>
2021-06-08 10:04:58 +0530

    commit;

---
+++ WebData/src/main/java/WebData/db/Db.java
@@ -0,0 +1,27 @@
+package WebData.db;
+
+import java.util.logging.Logger;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
+public class Db {
+	public static Logger log = Logs.getLog();
+	private static MongoDatabase md = null;
+	
+	public static MongoDatabase connect(){
+		if(md!=null)
+			return md;
+		MongoClient mo = new MongoClient("localhost",27017);
+		md = mo.getDatabase("mydb");
+		log.info("Connected to mydb lossg ok");
+		return md;
+	}
+	
+	public static void main(String args[]){
+		
+		MongoClient mo = new MongoClient("localhost",27017);
+		MongoDatabase md = mo.getDatabase("mydb");
+		System.out.println("connect success");
+	}
+}
